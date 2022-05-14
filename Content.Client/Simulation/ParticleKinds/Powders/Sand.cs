fec72f394a473927b69da11a64c2e7dcb10a9a90
ike709 <ike709@github.com>
2022-05-13 20:42:23 -0500

    Glass

--- Content.Client/Simulation/ParticleKinds/Powders/Sand.cs
+++ Content.Client/Simulation/ParticleKinds/Powders/Sand.cs
@@ -1,11 +1,13 @@
 ﻿using Content.Client.Simulation.ParticleKinds.Abstract;
 using Robust.Shared.Maths;
+using Robust.Shared.Random;
 
 namespace Content.Client.Simulation.ParticleKinds.Powders;
 
 [Particle]
 public sealed class Sand : ParticleImplementation
 {
+    [Dependency] private readonly IRobustRandom _random = default!;
     protected override ParticleType PType => ParticleType.Sand;
     protected override string PName => "Sand";
     protected override string PDescription => "A somewhat heavy dust.";
@@ -13,4 +15,13 @@ public sealed class Sand : ParticleImplementation
     protected override Color PColor => Color.SandyBrown;
     protected override ParticleMovementFlag PMovementFlags => ParticleMovementFlag.Spread;
     protected override ParticlePropertyFlag PPropertyFlags => ParticlePropertyFlag.None;
+    
+    public override void OnBurned(ref Particle self, ref Particle fire, uint selfId, uint fireId, Vector2i selfPosition,
+        Vector2i firePosition, Simulation sim)
+    {
+        if (_random.Prob(0.90f)) // TODO Replace with heat sim
+            return;
+        
+        sim.ChangeParticleType(selfId, selfPosition, ref self, ParticleType.Glass);
+    }
 }
