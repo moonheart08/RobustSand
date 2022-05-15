96dcb0fc34301c3bf6892b729625c4f3a8d28d4a
moonheart08 <moonheart08@users.noreply.github.com>
2022-05-15 12:24:17 -0500

    steam tweaks.

--- Content.Client/Simulation/ParticleKinds/Gas/Steam.cs
+++ Content.Client/Simulation/ParticleKinds/Gas/Steam.cs
@@ -9,7 +9,7 @@ public sealed class Steam : ParticleImplementation
     protected override ParticleType PType => ParticleType.Steam;
     protected override string PName => "Steam";
     protected override string PDescription => "Not the thing you're playing this from, if you even are.";
-    protected override byte PWeight => 13;
+    protected override byte PWeight => 15;
     protected override Color PColor => Color.WhiteSmoke;
     
     protected override float PRateOfGravity => -base.PRateOfGravity / 2;
@@ -18,7 +18,7 @@ public sealed class Steam : ParticleImplementation
     protected override float PSpecificHeat => 2.020f;
 
     protected override float PDiffusionRate => 0;
-    protected override ParticleMovementFlag PMovementFlags => ParticleMovementFlag.Gas;
+    protected override ParticleMovementFlag PMovementFlags => ParticleMovementFlag.Liquid;
     protected override ParticlePropertyFlag PPropertyFlags => ParticlePropertyFlag.Gas;
     protected override ParticleRenderFlag PParticleRenderFlags => ParticleRenderFlag.Blob;
 }
